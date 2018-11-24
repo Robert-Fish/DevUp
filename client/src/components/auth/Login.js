acd214e6cf4b8f1f63a06164d6237667e1b9f1a5
Robert-Fish <elasys63+github@gmail.com>
2018-11-24 10:51:19 +1000

    added more styling to login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,51 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+  }
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log(user);
+  };
+
   render() {
     return (
-      <div>
+      <div className="Login">
         <h1>Login</h1>
+        <form noValidate onSubmit={this.onSubmit}>
+          <input
+            placeholder="Email"
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.onChange}
+          />
+          <input
+            placeholder="Password"
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.onChange}
+          />
+          <input type="submit" className="btn btn-info btn-block mt-4" />
+        </form>
       </div>
     );
   }
