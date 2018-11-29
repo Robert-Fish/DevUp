210de5a1d63cffd0c59d4a360e5193854119008a
Robert-Fish <elasys63+github@gmail.com>
2018-11-29 15:14:59 +1000

    removed exp and edu

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -8,13 +8,13 @@ const ProfileActions = () => {
         <i className="fas fa-user-circle text-info mr-1" />
         Edit Profile
       </Link>
-      <Link className="btn btn-light" to="/add-experience">
+      {/* <Link className="btn btn-light" to="/add-experience">
         <i className="fab fa-black-tie text-info mr-1" /> Add Experience
       </Link>
       <Link className="btn btn-light" to="/add-education">
         <i className="fas fa-graduation-cap text-info mr-1" />
         Add Education
-      </Link>
+      </Link> */}
     </div>
   );
 };
