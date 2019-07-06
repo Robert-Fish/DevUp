3535bc357fe80f3d393fa8f8b0ca6702e29073f8
Robert Fish <elasys63+github@gmail.com>
2019-07-06 12:41:56 +1000

    removed unused prop type

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 // Redux
-import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 
 type Props ={
